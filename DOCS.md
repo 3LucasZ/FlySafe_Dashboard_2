893ff7961faebaa677f0bf3f7c15fce769cf1dbd
3LucasZ <lucas.zheng@warriorlife.net>
2023-11-22 13:55:35 -0800

    ok

--- DOCS.md
+++ DOCS.md
@@ -1,3 +1,4 @@
 ### UI
 
 - Tailwind
+  - npx tailwindcss -i FlySafe_Dashboard_2/input.css -o FlySafe_Dashboard_2/output.css --watch
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}"],
+  content: ["./**/*.{html,js}", "./index.html", "./pages/*"],
   theme: {
     colors: {
       blue: "#79e0ee",
