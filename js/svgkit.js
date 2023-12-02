bbc2af0ce0771caacd8104721ab620e780b8f9ae
3LucasZ <lucas.zheng@warriorlife.net>
2023-12-01 22:36:32 -0800

    /*html*/ ES6 Lintn Hack

---
+++ js/svgkit.js
@@ -0,0 +1,7 @@
+svgs = [];
+
+for (svg in svgs) {
+  var script = document.createElement("script");
+  script.src = "FlySafe_Dashboard_2/svgs/" + svg + ".js";
+  document.getElementsByClassName();
+}
