e829f32c3a17c6a830260d59be8a8d81fc56b50e
3LucasZ <lucas.zheng@warriorlife.net>
2023-12-01 23:34:02 -0800

    icons :\

--- js/svgkit.js
+++
@@ -1,7 +0,0 @@
-svgs = [];
-
-for (svg in svgs) {
-  var script = document.createElement("script");
-  script.src = "FlySafe_Dashboard_2/svgs/" + svg + ".js";
-  document.getElementsByClassName();
-}
