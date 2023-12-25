d5787f6e6beb903dd32b06a2c39caf8586d91c09
3LucasZ <lucas.zheng@warriorlife.net>
2023-12-24 16:12:51 -0800

    Create header.js

---
+++ components/header.js
@@ -0,0 +1,14 @@
+class Header extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.innerHTML = /*html*/ `
+  <div class="w-full block fixed inset-x-0 top-0 z-10 bg-gradient-to-b from-sky-300 to-cyan-200 text-center p-5">
+    FlySafe Dashboard
+  </div>
+`;
+  }
+}
+customElements.define("header-component", Header);
