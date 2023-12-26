39ac26b8d2a2aa42428e5c3645e7255cde78d2d1
3LucasZ <lucas.zheng@warriorlife.net>
2023-12-26 02:40:58 -0800

    fly page is iphone friendly now

--- js/fly_page.js
+++ js/fly_page.js
@@ -6,20 +6,20 @@ document.getElementById("flyPage").innerHTML = /*html*/ `
     >
       <div
         id="statusDiv"
-        class="bg-gradient-to-br from-red-500 to-red-300 p-4 rounded-lg text-center whitespace-nowrap"
+        class="bg-gradient-to-br from-red-500 to-red-300 p-4 rounded-lg text-center whitespace-nowrap truncate"
       >
         Not Connected
       </div>
       <div class="flex flex-row grid grid-cols-1 sm:grid-cols-2 gap-4">
         <div
           onclick="ws_reconnect()"
-          class="bg-gradient-to-br from-amber-400 to-amber-200 hover:scale-105 transition-all p-4 rounded-lg text-center"
+          class="bg-gradient-to-br from-amber-400 to-amber-200 hover:scale-105 transition-all p-4 rounded-lg text-center truncate"
         >
           Connect
         </div>
         <div
           onclick="ws_disconnect()"
-          class="bg-gradient-to-br from-amber-400 to-amber-200 hover:scale-105 transition-all p-4 rounded-lg text-center"
+          class="bg-gradient-to-br from-amber-400 to-amber-200 hover:scale-105 transition-all p-4 rounded-lg text-center truncate"
         >
           Disconnect
         </div>
@@ -28,7 +28,7 @@ document.getElementById("flyPage").innerHTML = /*html*/ `
     <div
       class="bg-gradient-to-br from-cyan-300 to-cyan-200 rounded shadow-lg p-4 col-span-5 relative flex items-center"
     >
-      <div class="text-center w-full">
+      <div class="text-center w-full whitespace-normal">
         <span class="text-8xl sm:text-9xl" id="distDiv">?</span>
         <span class="text-xl sm:text-2xl" id="distTypeDiv"></span>
       </div>
