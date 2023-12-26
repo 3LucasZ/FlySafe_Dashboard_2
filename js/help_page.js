610ce498f9ad00a97f139ab3159da323b937d281
3LucasZ <lucas.zheng@warriorlife.net>
2023-12-26 04:52:24 -0800

    finished help page (very lazy :D)

--- js/help_page.js
+++ js/help_page.js
@@ -1,7 +1,5 @@
 document.getElementById("helpPage").innerHTML = /*html*/ `
-<div>
-  <a href="https://docs.google.com/presentation/d/1E1LffRRFPVq6fAH2K6y-tBhzp-HUobneNcVZ-FFkUps/edit?usp=sharing" target="_blank" class="widget bg-orange" >Instruction Manual </a>
-</div>
+<a href="https://docs.google.com/presentation/d/1E1LffRRFPVq6fAH2K6y-tBhzp-HUobneNcVZ-FFkUps/edit?usp=sharing" target="_blank" class="bg-amber-100 screen-center p-4 rounded-lg" >Instruction Manual </a>
 `;
 
 //onload
