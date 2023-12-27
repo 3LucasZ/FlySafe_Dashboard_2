f856e0d0229b34f889ee8568bc3957c7250d6871
3LucasZ <lucas.zheng@warriorlife.net>
2023-12-26 20:31:18 -0800

    update static assets list for serviceWorker caching

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,24 +1,28 @@
 const repoName = "/FlySafe_Dashboard_2";
 
 //const cacheName = "cache_" + Math.floor(Math.random() * 100000000000000); //Local: auto cache refresh
-const cacheName = repoName + "v3987785"; //Production: manually change before every commit
+const cacheName = "2324328488"; //Production: manually change before every commit
 
 const assets = [
   repoName + "/",
   repoName + "/components/footer.js",
+  repoName + "/components/header.js",
   repoName + "/dist/output.css",
   repoName + "/images/icons/icon.png",
   repoName + "/js/chart.js",
   repoName + "/js/constants.js",
   repoName + "/js/createFileWorker.js",
+  repoName + "/js/fly_page.js",
+  repoName + "/js/fly.js",
   repoName + "/js/help_page.js",
-  repoName + "/js/index.js",
+  repoName + "/js/help.js",
   repoName + "/js/insights_page.js",
+  repoName + "/js/insights.js",
   repoName + "/js/settings_page.js",
+  repoName + "/js/settings.js",
   repoName + "/js/utils.js",
   repoName + "/index.html",
-  repoName + "/pages/settings_page.html",
-  repoName + "/pages/insights_page.html",
+  repoName + "/index.js",
 ];
 
 self.addEventListener("activate", (event) => {
