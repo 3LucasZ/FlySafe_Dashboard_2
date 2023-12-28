fb415a7b2cf4f351f61da84b0348dbf013779794
3LucasZ <lucas.zheng@warriorlife.net>
2023-12-27 18:24:59 -0800

    full screen presentation (very fancy sfull screen stylinnnn)

--- js/help_page.js
+++ js/help_page.js
@@ -1,12 +1,13 @@
 document.getElementById("helpPage").innerHTML = /*html*/ `
-<div class="w-[100vw] h-[56.4vw] p-0 m-0 border-solid border-4 border-sky-200 absolute z-10 pointer-events-none screen-center">
+<!--<div class="w-[100vw] h-[100vh] bg-cyan-200"></div>-->
+<div class="w-[100vw] h-[56.4vw] max-h-[calc(100vh_-_119px_-_env(safe-area-inset-bottom))] max-w-[calc((100vh_-_119px_-_env(safe-area-inset-bottom))_*_1.773)] border-solid border-4 border-sky-200 absolute z-10 screen-center pointer-events-none">
 </div>
 <iframe
   src="https://docs.google.com/presentation/d/e/2PACX-1vS8qcoyKA5y3HuSjIxn44E92S3sv6j7_6pvIywRxzp4r9s52EdZtdlQmQT5IoGABkqNNSMdq59gNJ_h/embed?start=false&loop=true&delayms=3000&rm=minimal"
   allowfullscreen="true"
   mozallowfullscreen="true"
   webkitallowfullscreen="true"
-  class="w-[100vw] h-[56.4vw] absolute z-0 screen-center"
+  class="w-[100vw] h-[56.4vw] max-h-[calc(100vh_-_119px_-_env(safe-area-inset-bottom))] max-w-[calc((100vh_-_119px_-_env(safe-area-inset-bottom))_*_1.773)] absolute z-0 screen-center"
 ></iframe>
 `;
 //onload
