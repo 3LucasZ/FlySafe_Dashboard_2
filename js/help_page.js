ed0c797303784342fbe03d1587799dbe8d6349e3
3LucasZ <lucas.zheng@warriorlife.net>
2023-12-27 17:34:09 -0800

    GOOGLE SLIDES INSTRUCTION MANUAL EMBED OMG

--- js/help_page.js
+++ js/help_page.js
@@ -1,7 +1,14 @@
 document.getElementById("helpPage").innerHTML = /*html*/ `
-<a href="https://docs.google.com/presentation/d/1E1LffRRFPVq6fAH2K6y-tBhzp-HUobneNcVZ-FFkUps/edit?usp=sharing" target="_blank" class="bg-amber-100 screen-center p-4 rounded-lg" >Instruction Manual </a>
+<div class="w-[100vw] h-[56.4vw] p-0 m-0 border-solid border-4 border-sky-200 absolute z-10 pointer-events-none screen-center">
+</div>
+<iframe
+  src="https://docs.google.com/presentation/d/e/2PACX-1vS8qcoyKA5y3HuSjIxn44E92S3sv6j7_6pvIywRxzp4r9s52EdZtdlQmQT5IoGABkqNNSMdq59gNJ_h/embed?start=false&loop=true&delayms=3000&rm=minimal"
+  allowfullscreen="true"
+  mozallowfullscreen="true"
+  webkitallowfullscreen="true"
+  class="w-[100vw] h-[56.4vw] absolute z-0 screen-center"
+></iframe>
 `;
-
 //onload
 function loadHelpPage() {
   changePage("helpPage");
